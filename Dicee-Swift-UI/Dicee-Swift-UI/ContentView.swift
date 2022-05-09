8aa6dd094263452c375d3c407be33156da2293ea
Rohit Raikhy <raikhy101@gmail.com>
2022-05-09 17:46:26 -0400

    Add files via upload

---
+++ Dicee-Swift-UI/Dicee-Swift-UI/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Dicee-Swift-UI
+//
+//  Created by Rohit Raikhy on 5/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    diceView(n:leftDiceNumber)
+                    diceView(n:rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct diceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Dicee-Swift-UI/Dicee-Swift-UI/Dicee_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_Swift_UIApp.swift
+//  Dicee-Swift-UI
+//
+//  Created by Rohit Raikhy on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
